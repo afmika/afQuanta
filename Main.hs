2970d8cf62bdb6712dcc55d3ebc033612e8ae12b
afmika <afmichael73@gmail.com>
2020-12-05 02:05:59 +0300

    QGate abstraction

--- Main.hs
+++ Main.hs
@@ -12,11 +12,11 @@ main =
 	let 
 		state  = qVec [1, 2, 1 :+ 1, 1]
 		transf = qEye (qVDim state)
+		gate   = QGate transf
 		psi    = qBit $ QVector [1 / sqrt 2, 1 / sqrt 2]
  	in
  		do {
 	 		putStrLn (show psi);
 	 		putStrLn ("N = " ++ (show $ qBitDim psi));
-		 	qShow transf;
-	 		qShow hadamard2;
+	 		qGateShow hadamard2;
 	 	}
--- Tools/QGates.hs
+++ Tools/QGates.hs
@@ -3,7 +3,25 @@ module QGates where
 import Data.Complex
 import QMatrix
 
-hadamard2 = (1 / sqrt 2) `times` QMatrix [
+data QGate = QGate (QMatrix) deriving (Show, Eq)
+
+qGateMatrix :: QGate -> QMatrix
+qGateMatrix (QGate mat) = mat
+
+-- @todo : unitary matrix control
+qGate :: QMatrix -> QGate
+qGate mat = QGate mat
+
+qGateDim :: QGate -> Int
+qGateDim (QGate mat) = qDim mat
+
+qGateShow (QGate qmat) = 
+	do {
+		putStrLn ("Gate dim = " ++ (show $ qGateDim (QGate qmat)));
+		qShow qmat;
+	}
+
+hadamard2 = qGate ((1 / sqrt 2) `times` QMatrix [
 			[1, 1],
 			[1, -1]
-		]
+		])
